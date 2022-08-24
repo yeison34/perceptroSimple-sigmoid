4d3225f5eb39f2a0433375cf3ef5cf83e223e4cd
yeison34 <jeisonperenguez1998@gmail.com>
2022-08-23 21:49:28 -0500

    perceptron simpre primera version

---
+++ src/perceptro/simple/Entrada.java
@@ -0,0 +1,21 @@
+
+package perceptro.simple;
+
+public class Entrada {
+    private double peso;
+    private double valor;
+    public Entrada(double valor,double peso){
+        this.peso=peso;
+        this.valor=valor;
+    }
+    
+    public double getPeso(){
+        return peso;
+    }
+    
+    public double getValor(){
+        return valor;
+    }
+    
+    
+}
